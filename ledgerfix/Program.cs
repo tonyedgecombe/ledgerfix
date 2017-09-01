c99d0c19ebe2ac52d98c6fa54384627b04f8f72f
Tony Edgecombe <tonyedgecombe@gmail.com>
2017-09-01 13:22:19 +0100

    Initial commit

---
+++ ledgerfix/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ledgerfix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string pathToLedger = @"C:\Data\ledger_3.1.1_win_bin\ledger.exe";
+
+            var newArgs = args.Select(arg => arg.StartsWith("C:\\") ? Path.GetFileName(arg) : arg)
+                              .Select(arg => arg.Contains(" ") ? $"\"{arg}\"" : arg);
+
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = pathToLedger,
+                    Arguments = string.Join(" ", newArgs),
+                    RedirectStandardOutput = true,
+                    RedirectStandardError= true,
+                    UseShellExecute = false,
+                }
+            };
+
+            process.Start();
+
+            process.OutputDataReceived += (sender, eventArgs) =>
+            {
+                Console.Write(eventArgs.Data);
+            };
+
+            process.ErrorDataReceived += (sender, eventArgs) =>
+            {
+                Console.Error.WriteLine(eventArgs.Data);
+            };
+
+            process.WaitForExit();
+        }
+    }
+}
